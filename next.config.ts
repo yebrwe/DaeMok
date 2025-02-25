fd607b666b9bc6c0dab7743293a424d9b1b6a93b
RheeHoonKyung <yebrwe@naver.com>
2025-02-25 22:24:26 +0900

    Fix: 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // 빌드 시 ESLint 실행을 비활성화
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
